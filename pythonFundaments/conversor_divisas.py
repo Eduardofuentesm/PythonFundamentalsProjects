9583e37a8ac1aeca620d572ba419e453780dc0cf
edufm <ofuentesm4@gmail.com>
2022-03-06 14:53:53 -0600

    Version 1 - Conversor de dolares a divisas

--- pythonFundaments/conversor_divisas.py
+++ pythonFundaments/conversor_divisas.py
@@ -1,23 +1,75 @@
+import os
+import sys
+import time
+
+
+def conversor_divisa(tipo_moneda, valor_dolar):
+    os.system('clear')
+    moneda = input('Cuantos Dolares US$  deseas convertir a ' + tipo_moneda+ ': ')
+    moneda = float(moneda)
+    dolares = moneda * valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    time.sleep(2)
+    os.system('clear')
+    print('Tienes ' + tipo_moneda + dolares)
+    time.sleep(2)
+    os.system('clear')
+    nuevo_intento = input('¿Deseas hacer nueva conversion?: (s/n): ')
+    if nuevo_intento == 's' or nuevo_intento == 'S':
+        os.system('clear')
+        run()
+    else:
+        os.system('clear')
+        print('\n\tHasta la vista Inteligente programador de python')
+        time.sleep(2)
+        os.system('clear')
+        sys.exit()
+
+
 def run():
+    os.system('clear')
     print('**¡Bienvenido al conversor de divisas!**\n\n')
+    time.sleep(2)
+    os.system('clear')
     print('Convertir de dolares a: \n')
-    divisas = {
+    divisas = """
 
-        'Euros': 1,
-        'Quetzales': 3,
-        'Pesos Argentinos': 4,
-        'Pesos Colombianos': 5,
-        'Pesos Mexicanos': 6,
-    }
-    for opciones in divisas.keys():
-        print(opciones)
+        1 - Euros
+        2 - Quetzales
+        3 - Pesos Argentinos
+        4 - Pesos Colombianos
+        5 - Pesos Mexicanos
+        6 - Salir del conversor
 
+        Elige una divisa:  """
 
-# def  run():
-#     print('**¡Bienvenido al conversor de divisas!**')
-#     moneda = int(input('¿Cuantos Dolares deseas cambiar?: ' ))
-#     moneda = int(moneda)
-
+    opciones = int(input(divisas))
+            
+    if opciones == 1:
+        conversor_divisa('Euros €', 0.91)
+    elif opciones == 2:
+        conversor_divisa('Quetzales Q', 7.75)
+    elif opciones == 3:
+        conversor_divisa('Pesos Argentinos ARP$a', 108.56)
+    elif opciones == 4:
+        conversor_divisa('Pesos Colombianos COL$', 3777.50)
+    elif opciones == 5:
+         conversor_divisa('Pesos Mexicanos $', 20.95)
+    elif opciones == 6:
+        os.system('clear')
+        print('\n\tHasta la vista Inteligente programador de python')
+        time.sleep(2)
+        os.system('clear')
+        sys.exit()
+    else:
+        os.system('clear')
+        print('\n\tIngrese una opcion valida por favor')
+        time.sleep(2)
+        os.system('clear')
+        run()
+        
     
+ 
 if __name__ == "__main__":
     run()
